384371c02440a9db3a6c7d896b88eccda54ff9bf
yanivamram <yaniv@lumen.me>
2025-07-27 23:16:30 +0300

    Create main.go

---
+++ server/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	pb "your/module/path/helloworld"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", req.GetName())
+	return &pb.HelloReply{Message: "Hello " + req.GetName()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+
+	log.Println("gRPC server listening on :50051")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
